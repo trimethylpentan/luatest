137e61361facebdc4b216e9c57487d513c92fb1f
Fischer1 <Fischer1-P@schueler.ers-hm.de>
2018-04-19 09:07:19 +0200

    Messages können angelegt werden

--- programs/notification/create-message.lua
+++ programs/notification/create-message.lua
@@ -2,6 +2,8 @@ local input = require("dependencies/input")
 local message = require("programs/notification/message")
 
 local user = input.getInput("Please enter the user you want to notify:", ".")
-message = message.create(user, "bla")
+local messageText = input.getInput("Please enter your message:", ".")
 
-io.write(message.userName .. "\n")
+newMessage = message.create(user, messageText)
+message.register(newMessage)
+io.write("Message succesfully created")
