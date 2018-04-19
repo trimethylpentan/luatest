eba072d423d346f7b536f9f5b16469b4949db6f4
Fischer1 <Fischer1-P@schueler.ers-hm.de>
2018-04-19 09:09:12 +0200

    Messages können angelegt werden

--- programs/notification/message.lua
+++ programs/notification/message.lua
@@ -1,12 +1,24 @@
 local table = require("std.table")
+local json = require("dependencies.json")
 
-local Message = {
-  userName = "",
-  message = ""
-}
+local messagesFilename = "./src/lua/programs/notification/data/messages.json"
+
+local file = io.open(messagesFilename, "r")
+local fileContent = file:read("*a")
+file:close()
+
+--all messages
+local messages = json.parse(fileContent)
+
+local Message = {}
+local singleMessage = {
+    userName = "",
+    message = ""
+  }
+
 
 local function create(userName, message)
-  local newMessage = table.clone(Message)
+  local newMessage = table.clone(singleMessage)
   newMessage.userName = userName
   newMessage.message = message
   
@@ -14,9 +26,18 @@ local function create(userName, message)
 end
 Message.create = create
 
-local function test()
-  return "test"
+local function register(message)
+  table.insert(messages, message)
+  for key, value in pairs(message) do
+    print(key)
+    print(value)
+  end
+  
+
+  local messagesFile = io.open(messagesFilename, "a")
+  messagesFile:write(json.stringify(message))
 end
-Message.test = test
+Message.register = register
+  
 
 return Message
